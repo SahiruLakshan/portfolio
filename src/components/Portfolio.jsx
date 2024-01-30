d41820d48ab5969ecac294370c6ea218b16d4937
Sahiru Lakshan <86883938+SahiruLakshan@users.noreply.github.com>
2024-01-30 16:45:57 +0530

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -56,7 +56,7 @@ const Portfolio = () => {
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-            Portfolio
+            Projects
           </p>
           <p className="py-6">Check out some of my work right here</p>
         </div>
